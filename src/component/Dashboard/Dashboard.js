a96a71e335cae15598127251733206cf183664a6
Murray Tucker <murraytucker@Murrays-MacBook-Pro.local>
2018-04-03 16:25:52 -0600

    initialize project

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div>
+            DASHBOARD
+            </div>
+        )
+    }
+}
